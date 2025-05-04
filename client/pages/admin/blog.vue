632ef516c62d2fd917028917874d4b992b5cc912
Arsehha <arseniymironov3116@gmail.com>
2025-05-05 00:20:37 +0300

    blog.vue

--- client/pages/admin/blog.vue
+++ client/pages/admin/blog.vue
@@ -1,11 +1,53 @@
 <template>
-  <div>
-    <v-data-table :headers="headers" :items="data">
-      <template v-slot:item.actions="{ item }">
-        <v-icon>mdi-pencil</v-icon>
-        <v-icon>mdi-delete</v-icon>
-      </template>
-    </v-data-table>
+  <div style="width: calc(100vw - 150px);">
+    <v-skeleton-loader v-if="loading" type="table"/>
+
+    <div class="d-flex flex-column" style="width: 100%; height: calc(100vh - 30px)" v-else>
+      <v-data-table :items="data"
+                    :headers="headers"
+                    hide-default-footer>
+        <!-- Опубликованно -->
+        <template v-slot:item.published="{ item }">
+          <td :class="'text-start ' + getCurrentColor(item.published)">
+            {{ item.published ? 'Да' : 'Нет' }}
+          </td>
+        </template>
+
+        <!-- Включен в слайдер -->
+        <template v-slot:item.includeSlider="{ item }">
+          <td :class="'text-start ' + getCurrentColor(item.includesSlider)">
+            {{ item.includeSlider ? 'Да' : 'Нет' }}
+          </td>
+        </template>
+
+        <!-- Предложенный -->
+        <template v-slot:item.suggested="{ item }">
+          <td :class="'text-start ' + getCurrentColor(item.suggested)">
+            {{ item.suggested ? 'Да' : 'Нет' }}
+          </td>
+        </template>
+
+        <!-- Дата создания -->
+        <template v-slot:item.created="{ item }">
+          <td class="text-start"> {{ getCreatedDate(item.created) }}</td>
+        </template>
+
+        <!-- Кнопки -->
+        <template v-slot:item.actions="{ item }">
+          <div class="d-flex">
+            <v-icon color="primary" class="mr-2">mdi-pencil</v-icon>
+            <admin-user-delete :item="item" @deleteUser="deletePost"/>
+          </div>
+        </template>
+      </v-data-table>
+
+      <vertical-spacer/>
+
+      <v-pagination v-model="pagPage"
+                    :length="pagSize"
+                    :total-visible="7"/>
+    </div>
+
   </div>
 </template>
 
@@ -21,15 +63,63 @@ import {Vue, Component} from 'vue-property-decorator';
   }
 })
 export default class Blog extends Vue {
+  loading: boolean = false;
+
+  page: number = 1;
+  size: number = 10;
+  pagPage: number = 1;
+
+  data: any = []
   headers: any = [
     {text: 'ID', value: 'id'},
-    {text: 'Дата регистрации', value: 'created'},
-    {text: '', value: 'actions', sortable: false},
+    {text: 'Заголовок', value: 'headLine'},
+    {text: 'ID создателя', value: 'user.id'},
+    {text: 'Входит в слайдер', value: 'includeSlider'},
+    {text: 'Опубликован', value: 'published'},
+    {text: 'Предложенный', value: 'suggested'},
+    {text: 'Дата создания', value: 'created'},
   ]
 
-  data: any = [
-    {}
-  ]
+  async mounted() {
+    this.loading = true;
+    await this.getPostList()
+
+    this.loading = false;
+  }
+
+  async getPostList() {
+    await this.$axios.get('/api/post/list')
+      .then((response) => {
+        this.data = response.data;
+      })
+  }
+
+  async deletePost(id: any) {
+    await this.$axios.delete('/api/post/delete/' + id)
+      .then((response) => {
+        this.getPostList()
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  getCreatedDate(created: Date) {
+    return new Date(created).toLocaleDateString("ru", {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    })
+  }
+
+  get pagSize(): number {
+    return Math.ceil(this.data.length / this.size)
+  }
+
+  getCurrentColor(value: boolean) {
+    return value ? 'green--text' : 'red--text'
+  }
+
 }
 </script>
 
