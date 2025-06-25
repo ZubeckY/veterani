1627cf8d86b68f9d0e06dc327caab96078723e8b
oleg <oleg_zub00@mail.ru>
2025-06-25 21:45:12 +0300

    test_1

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,10 @@ import config from './config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import controller from "./controllers";
-import connectDataBase from "./connectDb";
+import connectDataBase, {AppDataSource} from "./connectDb";
 import logger from "./modules/logger";
 import {setupSwagger} from "./swagger";
+import {File} from './entity'
 
 const app: express.Express = express()
 const port = config.port
@@ -20,6 +21,48 @@ app.use(bodyParser.urlencoded({extended: true,}))
 
 app.use('/api/media/', express.static('uploads'));
 app.use('/api/', controller)
+
+const fileWatcher = (switcher: boolean) => {
+    try {
+        const timer: number = 6 * 60 * 60 * 1000 // час * мин * сек * мс
+
+        const interval = setInterval(async () => {
+            if (!switcher) {
+                return clearInterval(interval);
+            }
+
+            await findUnusedFilesAndDelete()
+
+        }, timer)
+
+    } catch (error) {
+    }
+}
+
+
+async function findUnusedFilesAndDelete() {
+    try {
+        const fileRepository = AppDataSource.getRepository(File);
+        const files = await fileRepository.find({
+            where: {
+                used: false,
+            }
+        })
+
+        if (files.length === 0) {
+            return logger.info('\x1b[34m%s\x1b[0m', 'Неиспользуемые файлы не найдены') // Пока выводим в консоль, хз
+        }
+
+        const pathList = []
+        for (let i = 0; i < files.length; i++) {
+            let path = files[i].path
+            pathList.push(path)
+        }
+
+    } catch (error) {
+    }
+}
+
 
 app.listen(port, () => {
     try {
